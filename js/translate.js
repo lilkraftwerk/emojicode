9b990c503e1701500850da19668ba9c06f802fe7
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-07-14 15:17:26 -0700

    pretty background image

--- js/translate.js
+++ js/translate.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-  $("#text2emoji").on("click", function(){
-    var textToTranslate = getTextboxValue()
-    translateFromEnglishToEmoji(textToTranslate)
-  })
-  $("#emoji2text").on("click", function(){
-    var textToTranslate = getTextboxValue()
-    translateFromEmojiToEnglish(textToTranslate)
-  })
+$(document).ready(function() {
+    $("#text2emoji").on("click", function() {
+        var textToTranslate = getTextboxValue()
+        translateFromEnglishToEmoji(textToTranslate)
+    })
+    $("#emoji2text").on("click", function() {
+        var textToTranslate = getTextboxValue()
+        translateFromEmojiToEnglish(textToTranslate)
+    })
 })
 
 
@@ -27,83 +27,80 @@ var emojiToCodePoint = function(emoji) {
 }
 
 var formatCodePoint = function(codePoint) {
-  return "&#" + String(codePoint) + ";"
+    return "&#" + String(codePoint) + ";"
 }
 
 // emoji to english
-var translateFromEmojiToEnglish = function(inputString){
-  var splitString = emojiStringToArray(inputString)
-  var textArray = []
-  for(i=0; i<splitString.length; i++){
-    if(splitString[i] == " "){
-      textArray.push(splitString[i])
-    } else
-    {
-      var thisEnglishChar = returnEnglishCharFromEmoji(splitString[i])
-    textArray.push(thisEnglishChar)
+var translateFromEmojiToEnglish = function(inputString) {
+    var splitString = emojiStringToArray(inputString)
+    var textArray = []
+    for (i = 0; i < splitString.length; i++) {
+        if (splitString[i] == " ") {
+            textArray.push(splitString[i])
+        } else {
+            var thisEnglishChar = returnEnglishCharFromEmoji(splitString[i])
+            textArray.push(thisEnglishChar)
+        }
     }
-  }
-  var joinedString = textArray.join("")
-  deliverText(textArray)
+    var joinedString = textArray.join("")
+    deliverText(textArray)
 }
 
-var returnEnglishCharFromEmoji = function(emoji){
+var returnEnglishCharFromEmoji = function(emoji) {
     var thisCodePoint = emojiToCodePoint(emoji)
     var thisFormattedCode = formatCodePoint(thisCodePoint)
     return emojiToEnglish[thisFormattedCode]
 }
 
-var charCodeToDec = function(charCode){
-  return "&#" + String(charCode) + ";"
+var charCodeToDec = function(charCode) {
+    return "&#" + String(charCode) + ";"
 }
 
-var replaceEachEmojiInString = function(splitString){
-   for(i = 0; i < splitString.length; i++){
-    var thisEmoji = splitString[i]
-  }
-  return splitString.join("")
+var replaceEachEmojiInString = function(splitString) {
+    for (i = 0; i < splitString.length; i++) {
+        var thisEmoji = splitString[i]
+    }
+    return splitString.join("")
 }
 
 //get value in textbox
-var getTextboxValue = function(){
-  return $("#textbox").val()
+var getTextboxValue = function() {
+    return $("#textbox").val()
 }
 
 //split input string into array
-var splitInputString = function(inputString){
-  return inputString.split("")
+var splitInputString = function(inputString) {
+    return inputString.split("")
 }
 
 //for each in the split string, replace with random emoji from dictionary
 
-var getRandomEmojiFromArray = function(emojiArray){
-  var randomEmojiIndex = Math.floor(Math.random() * emojiArray.length)
-  return emojiArray[randomEmojiIndex]
+var getRandomEmojiFromArray = function(emojiArray) {
+    var randomEmojiIndex = Math.floor(Math.random() * emojiArray.length)
+    return emojiArray[randomEmojiIndex]
 }
 
-var replaceEachCharInString = function(splitString){
-  for(i = 0; i < splitString.length; i++){
-    var thisChar = splitString[i]
-    var thisEmojiArray = returnEmojiArray(thisChar)
-    var thisEmoji = getRandomEmojiFromArray(thisEmojiArray)
-    splitString[i] = thisEmoji
-  }
-  return splitString.join("")
+var replaceEachCharInString = function(splitString) {
+    for (i = 0; i < splitString.length; i++) {
+        var thisChar = splitString[i]
+        var thisEmojiArray = returnEmojiArray(thisChar)
+        var thisEmoji = getRandomEmojiFromArray(thisEmojiArray)
+        splitString[i] = thisEmoji
+    }
+    return splitString.join("")
 }
 
-var returnEmojiArray = function(character){
-  return englishToEmojiDictionary[character]
+var returnEmojiArray = function(character) {
+    return englishToEmojiDictionary[character]
 }
 
-var translateFromEnglishToEmoji = function(inputString){
-  var splitString = splitInputString(inputString)
-  var translatedSentence = replaceEachCharInString(splitString)
-  deliverText(translatedSentence)
-}
-
-
-var deliverText = function(valueToDeliver){
-  $("#delivery").html(valueToDeliver)
+var translateFromEnglishToEmoji = function(inputString) {
+    var splitString = splitInputString(inputString)
+    var translatedSentence = replaceEachCharInString(splitString)
+    deliverText(translatedSentence)
 }
 
 
+var deliverText = function(valueToDeliver) {
+    $("#delivery").html(valueToDeliver)
+}
